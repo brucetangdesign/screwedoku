c88092caeb70c704538cf32653de401b4b29f4bf
brucetangdesign <bruce@brucetangdesign.com>
2020-02-01 23:04:01 -0500

    fixed solved? method to look for any remianing 0s

--- board.rb
+++ board.rb
@@ -53,14 +53,14 @@ class Board
   alias_method :rows, :grid
 
   def solved?
-    rows.all? { |row| solved_set?(row) } &&
-      columns.all? { |col| solved_set?(col) } &&
+    #rows.all? { |row| solved_set?(row) } &&
+      #columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
   end
 
   def solved_set?(tiles)
     nums = tiles.map(&:value)
-    nums.uniq.length == nums.length
+    nums.uniq.length == nums.length && !nums.include?(0)
   end
 
   def square(idx)
